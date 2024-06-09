6ee675df8d14d96c057d2f11158f077c59b9a488
sssukh <sssukh50@gmail.com>
2024-06-09 23:47:56 +0900

    Adding Chest Property

---
+++ Source/SukProject/Prop/SukChest.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SukChest.generated.h"
+
+UCLASS()
+class SUKPROJECT_API ASukChest : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASukChest();
+
+
+};
